0db49a04f35fa3c300bbd9114f1475172d0234b3
Kris Austin <kris.austin@draft2digital.com>
2021-06-26 11:35:15 -0500

    Added Relay to admin

--- pool_droid/admin.py
+++ pool_droid/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+
 from pool_droid.models import Relay
 
-admin.site.register(Relay)
+
+@admin.register(Relay)
+class RelayAdmin(admin.ModelAdmin):
+    list_display = ["name", "gpio_bcm", "initial_state"]
