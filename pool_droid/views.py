95c1ec1c2bf20012ce8d8dc68508af9346d29376
Kris Austin <kris.austin@draft2digital.com>
2021-06-27 15:57:29 -0500

    Add OneWireTempSensor

--- pool_droid/views.py
+++ pool_droid/views.py
@@ -1,15 +1,21 @@
 from django.http import HttpResponse
 from django.views import View
 
-from pool_droid.models import Relay
+from pool_droid.models import Relay, OneWireTempSensor
 from pool_droid.utils import get_cabinet_temp, get_pump_speed
 
 
 class HomeView(View):
     def get(self, request):
         temp_c, temp_f = get_cabinet_temp()
+
+        temp_str = ""
+        for sensor in OneWireTempSensor.objects.all():
+            temp_str += f"{sensor.name}: {sensor.get_temp_f()} °F, "
+
         relay_str = ""
         for relay in Relay.objects.all():
             relay_str += f"{relay.name}: {relay.get_state()}, "
+
         pump_speed = get_pump_speed()
-        return HttpResponse(f"Cabinet Temp: {temp_c} °C, {temp_f} °F, {relay_str} Pump Speed: {pump_speed}")
+        return HttpResponse(f"{temp_str}{relay_str} Pump Speed: {pump_speed}")
