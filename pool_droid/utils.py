819cab321f3b0d6437e8c95ea503824049072cf7
Kris Austin <kris.austin@draft2digital.com>
2021-06-27 15:59:00 -0500

    Add OneWireTempSensor

--- pool_droid/utils.py
+++ pool_droid/utils.py
@@ -1,48 +1,7 @@
-import glob
-import os
-import time
-
 try:
     from pypentair import Pump
 except:
     pass
-
-
-def get_one_wire_temp_probe():
-    os.system('modprobe w1-gpio')
-    os.system('modprobe w1-therm')
-
-    base_dir = '/sys/bus/w1/devices/'
-    device_folder = glob.glob(base_dir + '28*')
-    if device_folder:
-        return device_folder[0] + '/w1_slave'
-    else:
-        return None
-
-
-def read_temp_raw(device_file):
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-
-def get_cabinet_temp():
-    device_file = get_one_wire_temp_probe()
-
-    if device_file:
-        lines = read_temp_raw(device_file)
-        while lines[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = read_temp_raw(device_file)
-        equals_pos = lines[1].find('t=')
-        if equals_pos != -1:
-            temp_string = lines[1][equals_pos + 2:]
-            temp_c = float(temp_string) / 1000.0
-            temp_f = temp_c * 9.0 / 5.0 + 32.0
-            return temp_c, temp_f
-    return None, None
-
 
 def get_pump_speed():
     return Pump(1).status["rpm"]
